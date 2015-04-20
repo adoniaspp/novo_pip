c2c24acd320c07e05d471ec34fad046bc462ef49
Simon <Simon@Simon>
2015-04-20 15:16:15 -0300

    Anuncio Datatable

--- visao/AnuncioVisaoListagemCadastrar.php
+++ visao/AnuncioVisaoListagemCadastrar.php
@@ -1,3 +1,7 @@
+
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.6/css/jquery.dataTables.css">
+<script src="assets/libs/DataTables-1.10.6/media/js/jquery.dataTables.min.js"></script>
+
 <div class="container">
     <div class="ui hidden divider"></div>
     <div class="ui page grid main">
@@ -13,31 +17,31 @@
     </div>
     <div class="ui hidden divider"></div>
 
-    <table class="ui table">
+    <table class="ui table" id="tabela">
         <thead>
             <tr>
                 <th>Tipo</th>
                 <th>Descrição</th>
                 <th>Data Cadastro</th>
                 <th>Detalhes</th>
-                <th>Operações</th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
 
             <?php
-            $params = array(
+           /* $params = array(
                 'mode' => 'Sliding',
                 'perPage' => 5,
                 'dela' => 2,
                 'itemData' => $this->getItem());
 
             $pager = & Pager::factory($params);
-            $data = $pager->getPageData();
+            $data = $pager->getPageData();*/
 
             Sessao::gerarToken();
 
-            foreach ($data as $imovel) {
+            foreach ($this->getItem() as $imovel) {
                 ?>
                 <tr>        
                     <?php
@@ -45,25 +49,27 @@
                     echo "<td>" . $imovel->getIdentificacao() . "</td>";
                     echo "<td>" . $imovel->getDatahoracadastro() . "</td>";
                     echo "<td><a href='#' class='ui green button' id='detalhes" . $imovel->getId() . "' ><i class='ui home icon'></i>Detalhes</div></td>";
+                    
+                    echo "<td>";
+                    
                     if (count($imovel->getAnuncio()) > 0 && verificaAnuncioAtivo($imovel->getAnuncio())) {
-                        echo"<td><div class='ui compact message'>Imóvel com Anúncio Ativo</div></td>";
+                        echo"<div class='ui compact message'>Imóvel com Anúncio Ativo</div>";
                     } else {
-                        echo"<td><a href='index.php?entidade=Anuncio&acao=form&idImovel=" . $imovel->getId() . "&token=" . $_SESSION['token'] . "' class='btn btn-info'><div class='ui purple button'>Publicar Anúncio</div></a></td>";
+                        echo"<a href='index.php?entidade=Anuncio&acao=form&idImovel=" . $imovel->getId() . "&token=" . $_SESSION['token'] . "' class='btn btn-info'><div class='ui purple button'>Publicar Anúncio</div></a>";
                     }
+                    
+                    echo "</td>";
                 }
                 ?>                    
             </tr>         
         </tbody>
     </table>
-    <?php
-    $links = $pager->getLinks();
-    echo ($links['all'] != "" ? "&nbsp;&nbsp;&nbsp;&nbsp;Página: " . $links['all'] : "");
-    ?>
 </div>
 <div class="ui hidden divider"></div>
 
 <?php
 include_once "/modal/ImovelListagemModal.php";
+
 
 function verificaAnuncioAtivo($listaAnuncios) {
     $temAnuncioAtivo = false;
@@ -79,3 +85,17 @@ function verificaAnuncioAtivo($listaAnuncios) {
     return $temAnuncioAtivo;
 }
 ?>
+
+<script>
+$('#tabela').dataTable({
+        "language": {
+            "url": "assets/libs/DataTables-1.10.6/media/js/pt.json",
+        },
+        "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "Todos"]],
+        "stateSave": true,
+        "columnDefs": [
+        { "orderable": false, "targets": 3 },
+        { "orderable": false, "targets": 4 }
+        ]
+    });
+</script>
