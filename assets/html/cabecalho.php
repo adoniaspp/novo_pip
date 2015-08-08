5bc1f56c88d1fa55117972a3dc801fba1405adb5
Simon <Simon@Simon>
2015-08-08 10:10:25 -0300

    Cadastro Usuario, Cabeçaho, Js, Captcha Incompleto e Outros

--- assets/html/cabecalho.php
+++ assets/html/cabecalho.php
@@ -2,6 +2,7 @@
 <!DOCTYPE HTML>
 <html lang="pt-br">
     <head>
+        
         <meta charset="UTF-8">
         <title>PIP - Procure Im&oacute;veis Pai D'Egua</title> 
         <!-- JQUERY --> 
@@ -10,81 +11,63 @@
         <link href="<?php echo PIPURL; ?>/assets/libs/semantic-ui/semantic.min.css" rel="stylesheet" type="text/css" /> 
         <script src="<?php echo PIPURL; ?>/assets/libs/semantic-ui/semantic.min.js"></script> 
 
-        <script src="<?php echo PIPURL; ?>/assets/js/cabecalho.js"></script> 
+        <script src="assets/js/usuario.js"></script>
         <!-- Template PIP -->
         <link href="<?php echo PIPURL; ?>/assets/css/template-pip.css" rel="stylesheet" type="text/css" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">        
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">        
     </head>
 
 <script>
-        esconderMeuPIP();
-
-        $(document).ready(function () {
-            digitarLoginSenha();
+ $(document).ready(function() {
 <?php
-if (Sessao::verificarSessaoUsuario()) {
-    ?>
-                exibirMeuPIP();
-                $("#divNome").html("<font color='black'><h4>Seja bem vindo, <?php echo ucfirst(strtolower(explode(" ", $_SESSION['nome'])[0])); ?>  <h4></font>");
-    <?php
-} else {
-    //Sessao::gerarToken();
-};
-?>
-            $("#btnAcessar").click(function () {
-                autenticarUsuario();
-            });
-            $("#btnLogout").click(function () {
-                logoutUsuario();
-            });
-        });
-
+if (Sessao::verificarSessaoUsuario()) { ?>
+     exibirMeuPIP("SIM", "<?php echo ucfirst(strtolower(explode(" ", $_SESSION['nome'])[0])); ?>");
+     logout();
+<?php } else {?>
+    
+    exibirMeuPIP("NAO", "");
+    
+<?php }?>
+})  
 </script>
 
-    <body>   
         <div class="container">  
-            <nav  class="ui menu inverted navbar page grid" style="background-color: #ffffff" style="margin-bottom:300px">
+            <nav class="ui menu inverted navbar page grid" style="background-color: #ffffff" style="margin-bottom:300px">
                 <a href="<?php echo PIPURL; ?>/index.php"> <img src="<?php echo PIPURL; ?>/assets/imagens/logo.png" width="120px"/></a>
-             
-                <div id="divForm" class="right menu hide">
-                    
+               
+            <div id="loginCadastro">
                 
-                    <div class="ui form">
-                    <div class="fields">
-                      <div class="five field">                            
-                        <label>Login</label>                        
-                        <input placeholder="Digite o Login" type="text" id="txtLoginIndex" name="txtLoginIndex">                       
-                      </div>
-                      <div class="field">
-                        <label>Senha</label>
-                        <input placeholder="Digite a Senha" type="password" id="txtSenhaIndex" name="txtSenhaIndex">
-                        </div>
-                      </div>
-                      <div class="field">
-                          <label style="color: white">Teste</label>
-                        <button class="ui green button" id="btnAcessar">Fazer Login</button>
-                         <a href="<?php echo PIPURL; ?>/index.php?entidade=Usuario&acao=form&tipo=cadastro" class="ui green label">Ainda não é cadastrado?</a>                      
-                       <a href="<?php echo PIPURL; ?>/index.php?entidade=Usuario&acao=form&tipo=esquecisenha" class="ui red label">Não lembra a senha?</a>
-                      </div>
+                <div class="ui menu">
+                    <div class="item">
+                        <a href="<?php echo PIPURL; ?>/index.php?entidade=Usuario&acao=form&tipo=login"><div class="ui primary button" id="btnAcessar">LOGIN</div></a>
                     </div>
-                </div>
+                    <div class="item">
+                        <a href="<?php echo PIPURL; ?>/index.php?entidade=Usuario&acao=form&tipo=cadastro"<div class="ui button">CADASTRAR-SE</div></a>
+                    </div>
+            </div>
+                    
+        </div>
+                <!--<label for="CaptchaCode">Retype the characters from the picture:</label>
+<?php // Adding BotDetect Captcha to the page 
+  /*$SampleCaptcha = new Captcha("SampleCaptcha");
+  $SampleCaptcha->UserInputID = "CaptchaCode";
+  echo $SampleCaptcha->Html(); */
+?>
 
-              </div>
-
-                <div id="divUsuario" class="right menu hide" style="margin-left:800px; margin-top: -80px">
-                    <form>
+<input name="CaptchaCode" id="CaptchaCode" type="text" />-->
+                
+                
+            <div id="divUsuario" class="right menu hide" style="margin-left:800px; margin-top: -80px">
+                <form>
                            
-                        <div class="ui center aligned compact segment" style="margin-bottom: 10px">
-                            <div id="divNome" style="margin-bottom: -10px"> </div>
-                            <div>
+                    <div class="ui center aligned compact segment" style="margin-bottom: 10px">
+                        <div id="divNome" style="margin-bottom: -10px"> </div>
+                        <div>
                             <a href="<?php echo PIPURL; ?>/index.php?entidade=Usuario&acao=meuPIP" class="ui primary button"> <i class="block layout icon"></i> Meu PIP </a>
                             <a id="btnLogout" href="#" class="ui red button"><i class="power icon"></i> Sair</a>  
-                            </div>
                         </div>
+                    </div>
                         
                         
-                    </form>            
-                </div>
-
-            </nav>
-        </div>
+                </form>            
+            </div>
