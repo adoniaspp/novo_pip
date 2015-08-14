d6b774bfc17afb88be0f24b6d1a7521a2d9ac2f7
GEORGE <GEORGE@GEORGE-PC>
2015-08-14 20:48:18 -0300

    procedure limparbase

---
+++ views/procedures.sql
@@ -0,0 +1,30 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `limparbase`()
+BEGIN
+ SET FOREIGN_KEY_CHECKS=0;
+truncate table valor;
+truncate table planta;
+truncate table apartamentoplanta;
+truncate table apartamento;
+truncate table casa;
+truncate table kitnet;
+truncate table prediocomercial;
+truncate table salacomercial;
+truncate table terreno;
+
+truncate table imoveldiferencial;
+truncate table endereco;
+truncate table emailanuncio;
+truncate table historicoaluguelvenda;
+truncate table imagem;
+truncate table respostamensagem;
+truncate table mensagem;
+truncate table anuncioclique;
+truncate table anuncio;
+
+truncate table telefone;
+truncate table imovel;
+truncate table empresa;
+truncate table recuperasenha;
+truncate table usuarioplano;
+truncate table usuario;
+END
