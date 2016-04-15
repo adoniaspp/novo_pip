84eb6743b1d7fe753b55d723ffe3c0ec78d7fd69
adoniaspp <adoniaspp@ADONIAS>
2016-04-15 17:55:13 -0300

    Preferências

--- configuracao/cookies.php
+++ configuracao/cookies.php
@@ -1,20 +1,43 @@
 <?php
 
 include_once 'controle/PreferenciaControle.php';
+include_once 'DAO/ConsultasAdHoc.php';
 
 class Cookies {
 
     public static function consultarPreferencias() {
-//        $idsanuncio = array();
+        $idsanuncio = array();
         if (isset($_COOKIE['pip'])) {
-//            foreach ($_COOKIE['pip'] as $nome => $valor) {
-//                array_push($idsanuncio, $valor);
-//            }
-            
-            //var_dump($_COOKIE['pip']);
-            //die();
+            foreach ($_COOKIE['pip'] as $nome => $valor) {
+                array_push($idsanuncio, $valor);
+            }  
+        $predicados["idanuncio"] = $idsanuncio;
+        $consultasAdHoc = new ConsultasAdHoc();
+        $parametros["atributos"] = "idanuncio, finalidade, novovalor, valormin, tipo, bairro,"
+                . "cidade, estado";
+        $parametros["tabela"] = "todos";
+        $parametros["predicados"] = $predicados;
+        $listaAnuncio = $consultasAdHoc->buscaAnuncios($parametros);
+//        echo '<pre>';
+//        print_r($listaAnuncio[anuncio]);
+//        echo '</pre>';
+//        die();
+        $parametros["atributos"] = "*";
+        $parametros["tabela"] = "todos";
+        $predicados = array();
+        foreach ($listaAnuncio[anuncio] as $anuncios) {
+            $predicados = $predicados + $anuncios;
+            foreach ($anuncios as $nome => $valor) {
+                $predicados[$nome] = array($valor);
+            }
         }
-        return false;
+//        echo '<pre>';
+//        print_r($result);
+//        echo '</pre>';
+//        die();
+//        $parametros["predicados"] = $predicados;
+//        $listaAnuncio = $consultasAdHoc->buscaAnuncios($parametros);
+        }
     }
 
     public static function configurarPreferencias($parametros) {
