ac450ce5fba5198dc365bb8643f04f5c3db3c535
Adonias <Adonias@LAPTOP-NKSTB63E>
2018-05-04 18:08:55 -0300

    Dúvidas Frequentes

--- visao/InstitucionalDuvidasFrequentes.php
+++ visao/InstitucionalDuvidasFrequentes.php
@@ -22,7 +22,7 @@
                 Caso a validade do meu anúncio expire e necessite ainda manter o meu anúncio no site, terei que recadastrar meu imóvel?
             </div>
             <div class="content">
-                <p>Não, você precisa apenas revalidar seu anuncio a partir da comprar de um de nossos planos.</p>
+                <p>Não, você precisa apenas revalidar seu anuncio a partir da compra de um de nossos planos.</p>
             </div>
             <div class="title">
                 <i class="dropdown icon"></i>
@@ -30,6 +30,55 @@
             </div>
             <div class="content">
                 <p>Não, você pode cadastrar quantos imóveis desejar e posteriormente anunciar conforme o seu plano de anúncios.</p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Existe limite na compra de planos de anúncio?
+            </div>
+            <div class="content">
+                <p>Não, você pode comprar quantos planos desejar e utilizar-los individualmente para cada imóvel cadastrado.</p>
+            </div>
+                <div class="title">
+                <i class="dropdown icon"></i>
+                É cobrada alguma taxa adicional para compartilhar meus anúncios em redes sociais?
+            </div>
+            <div class="content">
+                <p>Não, qualquer tipo de anúncio, no PIP Online, pode ser compartilhado nas redes sociais sem cobrança de qualquer taxa adicional.</p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Depois de publicar meu anúncio, posso fazer alterações em seu conteúdo?
+            </div>
+            <div class="content">
+                <p>Sim, você pode alterar qualquer dado do seu anúncio, com exceção do tipo do imóvel.</p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Tenho um anúncio de locação de um imóvel no site, este expirou e preciso continuar anunciando o imóvel. Nesse caso, preciso recadastrar o anúncio?
+            </div>
+            <div class="content">
+                <p>Não, nesse caso, apenas utilize a função reativar anúncio aluguel e pronto, você apenas precisa ter um plano de anúncio ativo.</p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Meu anúncio não foi aprovado para publicação, o que devo fazer agora?
+            </div>
+            <div class="content">
+                <p>Verifique os temos de uso do PIP Online e tente cadastra-lo novamente.</p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Comprei equivocadamente um plano de anúncio, o que devo fazer?
+            </div>
+            <div class="content">
+                <p>Em até 7 dias, entre em contato com o PIP Online através de nossa Central de Relacionamento.</p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Vendi ou aluguei meu imóvel e não desejo mais manter o anúncio visível no site, o que devo fazer?
+            </div>
+            <div class="content">
+                <p>É simples, basta acessar a opção Anúncios Ativos, escolher o anúncio que deseja finalizar e clicar no botão Finalizar Anúncio.</p>
             </div>
         </div>
 
