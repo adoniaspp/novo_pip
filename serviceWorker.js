f82fda6330b8f05925b034fe0e58afacfd98aeb0
adoniaspp <adoniaspp@gmail.com>
2018-12-18 19:10:14 -0300

    Alteração pwa

--- serviceWorker.js
+++ serviceWorker.js
@@ -15,3 +15,16 @@ self.addEventListener('install', function(event) {
             })
     )
 });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+    );
+});
